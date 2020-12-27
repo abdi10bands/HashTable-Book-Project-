c7b513892f40cc78cdf4a14ac6c5b82c6dab2a56
abdi10bands <76499323+abdi10bands@users.noreply.github.com>
2020-12-27 01:06:18 -0800

    Update llist.h

--- llist.h
+++ llist.h
@@ -1,11 +1,6 @@
-//CS311 Yoshii
-//INSTRUCTION:
-//Llist class - header file template 
-//You must complete the ** parts and then complete llist.cpp
-//Don't forget PURPOSE and PARAMETERS 
 
 // =======================================================
-// HW#: HW3P1 llist
+
 // Your name: Abdiel
 // Compiler:  g++ 
 // File type: headher file  llist.h
